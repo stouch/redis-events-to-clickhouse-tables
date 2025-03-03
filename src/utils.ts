499eb3948072f1c7b9ba00a33cd4ccc00a5b2d7e
Sylvain Touchard <syl20.touchard@gmail.com>
2025-03-03 17:53:32 +0100

    fix: Improve date type 2/2

--- src/utils.ts
+++ src/utils.ts
@@ -2,6 +2,6 @@ import { dayjs } from "./dayjs-utc.js";
 
 export const isDateString = (str: string) => {
   return (
-    str.trim().match(/^([0-9]{2}([0-9]{2})?(\/|-)|)+/gi) && dayjs(str).isValid()
+    str.trim().match(/^([0-9]{2}([0-9]{2})?(\/|-))+/gi) && dayjs(str).isValid()
   );
 };
